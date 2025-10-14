30afbe4262b49b64c732f7db30d86f11bb62bcc2
ChenYiNong <chenyn2025@lzu.edu.cn>
2025-10-14 10:10:41 +0800

    prob7 not finished yet

---
+++ test_a/prob7_biggeest_decimal/max.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,b,c,d;
+    printf("enter 4 numbers between 0 and 9,avoid 0 0 0 0:");
+    scanf("%d %d %d %d",&a,&b,&c,&d);
+    for (int i = 0; i < 3 ; i++)
+    {
+        
+    }
+}
