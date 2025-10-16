1c222c4a55892dc8487ffda588ec479799611a51
ChenYiNong <chenyn2025@lzu.edu.cn>
2025-10-16 08:43:02 +0800

    solve prob7 quickly

--- test_a/prob7_biggeest_decimal/max.c
+++ test_a/prob7_biggeest_decimal/max.c
@@ -2,11 +2,22 @@
 
 int main()
 {
-    int a,b,c,d;
+    int a[4],t;
     printf("enter 4 numbers between 0 and 9,avoid 0 0 0 0:");
-    scanf("%d %d %d %d",&a,&b,&c,&d);
+    scanf("%d %d %d %d",,&a[1],&a[2],&a[3]);
     for (int i = 0; i < 3 ; i++)
     {
+        for (int j = 0; j < 3; j++)
+        {
+            if(a[j] > a[j+1])
+            {
+                t = a[j];
+                a[j] = a[j+1];
+                a[j+1] = t;
+            }
+        }
         
     }
+    printf("%d%d%d%d",a[3],a[2],a[1],a[0]);
+    return 0;
 }
